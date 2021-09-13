56a2e873dfe6f85a386372a1d633927c2fc127a9
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-09-13 15:13:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -52,19 +52,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
-const grid = document.querySelector('.grid');
+    const grid = document.querySelector('.grid')
+    const cardsChosen = []
+    const cardsChosenId = []
 
-//funciton to create board
-function createBoard() {
-    for (let i = 0; i < cardArray.length; i++) {
-        const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
-        card.setAttribute('data-id', i);
-        // card.addEventListener('click', flipcard())
-        grid.appendChild(card);
+    //funciton to create board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            // card.addEventListener('click', flipcard())
+            grid.appendChild(card)
+        }
     }
-}
 
-createBoard()
+    //check for matches
+
+
+    //flip the card
+    function flipCard() {
+        const cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', card[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+
+    createBoard()
 
 });
